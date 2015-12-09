ffc4f50d57f7b19af74e4362209c1afe9537f05d
Michal Janiszewski <mjaniszew@gmail.com>
2015-12-09 23:36:20 +0100

    First window

---
+++ src/index.js
@@ -0,0 +1,14 @@
+var app    = require('app');
+var Window = require('browser-window');
+
+app.on('ready', function () {
+    var mainWindow = new Window({
+        width: 800,
+        height: 600
+    });
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+});
+
+app.on('window-all-closed', function () {
+    app.quit();
+});
